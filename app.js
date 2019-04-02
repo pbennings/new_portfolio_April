257e917d0b320b999fd584dac9bba40f3e55b6ae
Paul Bennings <paul.bennings@codeimmersives.com>
2019-04-02 12:51:05 -0400

    added js objects

--- app.js
+++ app.js
@@ -5,3 +5,58 @@ const result = document.getElementById('result');
   result.innerHTML = 'Hi ' + form.fullName.value;(form.fullname.value);
   this.reset();
   };
+
+  const resume = {
+    person: {
+      name: 'Paul Bennings',
+      email: 'paul@happnyc.com',
+      number: '702-913-4907',
+      social media: {
+        Instagram: 'www.instagram.com/heavyweight_nyc'
+        Facebook: 'www.facebook.com/heavyweight.nyc/'
+      }, 
+    experience: {
+      job1: {
+        nameofjob: '',
+        title: '',
+        lengtoftime: '',
+        description: {
+          achievements: ''
+        },
+    
+      job2: {
+        nameofjob: '',
+        title: '',
+        lengthoftime: '',
+        description: {
+          achievements: ''
+        },
+   }
+  },
+   projects: {
+      project1: '',
+        nameofproject: '',
+        link: '',
+        screenshot: '',
+        github: '',
+        achievements: ''
+},
+projects2: {
+  project1: '',
+    nameofproject: '',
+    link: '',
+    screenshot: '',
+    github: '',
+    achievements: ''
+},   
+projects3: {
+  project1: '',
+    nameofproject: '',
+    link: '',
+    screenshot: '',
+    github: '',
+    achievements: ''
+},
+skills: ['Javascript','HTML','CSS'],
+  },
+    }
